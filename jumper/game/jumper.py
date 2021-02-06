e5bb686ca6fae556ad58798432c3c5e230c3d944
Nolan Meacham <nolan.meacham@gmail.com>
2021-02-05 19:01:23 -0700

    Updates to jumper class

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -4,7 +4,7 @@ class Jumper():
     """Class that holds information regarding the jumper.
 
     Stereotype:
-        ???
+        Information Holder
 
     Attributes:
         self.num_guesses (int): counts the number of times the user has guessed a letter.
@@ -18,15 +18,15 @@ class Jumper():
         self.num_guesses = 0
         self.is_alive = True
         self.head = 'O'
-        self.chute =['    ___',
-                     '  /     \ ',
-                     ' /_______\ ',
-                     ' \       / ',
-                     '  \     /  ',
-                     '   \   /',
-                     f'     {self.head}    ',
-                     '    /|\   ',
-                     '    / \   ']
+        self.chute = ['    ___',
+                      '  /     \ ',
+                      ' /_______\ ',
+                      ' \       / ',
+                      '  \     /  ',
+                      '   \   /',
+                      f'     {self.head}    ',
+                      '    /|\   ',
+                      '    / \   ']
 
     def update_chute(self, result):
         """
@@ -42,15 +42,29 @@ class Jumper():
             chute (list): each line of the chute
 
         """
-        # This is being used right now just to test the chute display
-        for line in self.chute:
-            print(line)
+        self.num_guesses += 1
+        if self.num_guesses > 6:
+            self.is_alive = False
 
         # TODO: If result is True, the chute doesn't change. If result is Fale, the chute is reduced by one line.
+        if result == False:
+            self.chute.pop(0)
 
         # TODO: If is_alive is True, self.head = 'O', otherwise, self.head = 'X'
+        if self.is_alive == False:
+            self.chute[-3] = '     X    '
+
+        # This is being used right now just to test the chute display
+        # for line in self.chute:
+        #     print(line)
+
+        return self.chute
+
 
 
 # # Test
 # jumper = Jumper()
 # jumper.update_chute(True)
+
+# for line in jumper.chute:
+#     print(line)
