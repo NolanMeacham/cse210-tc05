d94fa1b075a4fc7c7a676dbcd3cb52b2126e1694
Nolan Meacham <nolan.meacham@gmail.com>
2021-02-08 15:05:02 -0700

    modified display, shows finished word if you win and spacing between jumper and hidden word

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -37,14 +37,16 @@ class Director:
         Asks the user input
         """
         
-        
+        self.console.write('')
         self.console.write(self.word_processor.hidden_word)
+        self.console.write('')
         
         self.console.write(self.jumper.displayed_chute)
         self.console.write('---------------')
         
 
         self.letter = self.console.read("Guess a letter [A-Z]: ")
+        self.console.write('')
         
 
     def do_updates(self):
@@ -54,11 +56,12 @@ class Director:
         """
         self.jumper.update_chute(self.word_processor.check_input(self.letter))
         if self.word_processor.check_complete():
+            self.console.write(self.word_processor.hidden_word)
             self.console.write("You win!\nThanks for playing!")
             self.keep_playing == False
             exit()
         elif self.jumper.is_alive is False:
-            self.console.write(f'Sorry the word was {self.word_processor.word_choice}')
+            self.console.write(f'Sorry the word was: {self.word_processor.word_choice}')
             self.console.google_word(self.word_processor.word_choice)
             self.console.write('Oh. He ded...')
             self.console.write(self.jumper.displayed_chute)
